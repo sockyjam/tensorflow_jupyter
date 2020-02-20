107743178be3182111e13652de157bd8f8055279
sockyjam@126.com <sockyjam@126.com>
2020-02-20 12:55:58 +0800

    aa

--- notebooks/yolo_2/xml_to_csv.py
+++ notebooks/yolo_2/xml_to_csv.py
@@ -9,30 +9,35 @@ def xml_to_csv(path):
     for xml_file in glob.glob(path + '/*.xml'):
         tree = ET.parse(xml_file)
         root = tree.getroot()
+
+        value = ["sss", "./image/train/"+root.find('filename').text]
+
         for member in root.findall('object'):
-            value = (root.find('filename').text,
-                     int(root.find('size')[0].text),
-                     int(root.find('size')[1].text),
-                     member[0].text,
-                     int(member[4][0].text),
-                     int(member[4][1].text),
-                     int(member[4][2].text),
-                     int(member[4][3].text)
-                     )
-            xml_list.append(value)
-    column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
-    xml_df = pd.DataFrame(xml_list, columns=column_name)
+            if member[0].text == 'iphone':
+                class_id = 0
+            elif member[0].text == 'kindle':
+                class_id = 1
+            elif member[0].text == 'mouse':
+                class_id = 2
+            value.append(int(member[4][0].text))
+            value.append(int(member[4][1].text))
+            value.append(int(member[4][2].text))
+            value.append(int(member[4][3].text))
+            value.append(class_id)
+            value.append(" +++ ")
+        xml_list.append(value)
+    # column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    xml_df = pd.DataFrame(xml_list)
     return xml_df
 
 
 def main():
-    for folder in ['train', 'test']:
-        image_path = os.path.join(os.getcwd(), ('images/' + folder))
+    for folder in ['train']:
+        image_path = os.path.join(os.getcwd(), ('image/' + folder))
         xml_df = xml_to_csv(image_path)
-        xml_df.to_csv(('images/'+folder+'_labels.csv'), index=None)
+        xml_df.to_csv(('image/'+folder+'_labels.csv'), index=None)
     print('Successfully converted xml to csv.')
 
 if __name__ == '__main__':
     main()
-    
-    
+
